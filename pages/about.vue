d927fc87bc560c1b5687350e7045abc733510bf2
Gery Mont <gerymont@gmail.com>
2017-07-29 16:37:49 -0300

    Agregado un about y linkeado en el home y viceversa

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>Acerca de nosotros</p>
+    <nuxt-link to="/">Volver a inicio</nuxt-link>
+  </div>
+</template>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@
         Universal Vue.js Application
       </h2>
       <div class="links">
+        <nuxt-link to="/about"  class="button--green">Acerca de nosotros</nuxt-link>
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
